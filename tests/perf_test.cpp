9c265e68274fd969e118217ba6810e392ba061bf
Harry van Haaren <harryhaaren@gmail.com>
2017-09-04 18:44:17 +0100

    tests: reworking tests to run with meson/ninja
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- tests/perf_test.cpp
+++ tests/perf_test.cpp
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "benchmark/benchmark.h"
 
 #include <avtka/avtka.h>
@@ -11,11 +13,10 @@ event_cb(struct avtka_t *avtka, uint32_t item, float v, void *userdata)
 void create_destroy(benchmark::State& state)
 {
 	while (state.KeepRunning()) {
-		struct avtka_opts_t opts = {
-			.w = 360,
-			.h = 240,
-			.event_callback = event_cb,
-		};
+		struct avtka_opts_t opts = {0};
+		opts.w = 360;
+		opts.h = 240;
+		opts.event_callback = event_cb;
 		struct avtka_t *a = avtka_create("Avtka Perf Test", &opts);
 		avtka_iterate(a);
 		avtka_destroy(a);
@@ -24,23 +25,22 @@ void create_destroy(benchmark::State& state)
 
 void iterate_basic(benchmark::State& state)
 {
-	struct avtka_opts_t opts = {
-		.w = 360,
-		.h = 240,
-		.event_callback = event_cb,
-	};
+	struct avtka_opts_t opts = {0};
+	opts.w = 360;
+	opts.h = 240;
+	opts.event_callback = event_cb;
 	struct avtka_t *a = avtka_create("Avtka Perf Test", &opts);
 
 	struct avtka_item_opts_t item = {
-		.name = "Dial 1",
 		.x = 10, .y = 10, .w = 50, .h = 50,
-		.draw = AVTKA_DRAW_DIAL,
-		.interact = AVTKA_INTERACT_DRAG_V,
 	};
-	uint32_t i1 = avtka_item_create(a, &item);
+	snprintf(item.name, sizeof(item.name), "item");
+
 #warning > 1000 items causes segfault in puglDestroy - investigate
-	for(int i = 0; i < 1000; i++) {
-		item.draw = i % 3;
+	for(int i = 0; i < state.range(0); i++) {
+		item.draw = state.range(1);
+		item.x = rand() % 360;
+		item.y = rand() % 240;
 		avtka_item_create(a, &item);
 	}
 
@@ -53,9 +53,14 @@ void iterate_basic(benchmark::State& state)
 	avtka_destroy(a);
 }
 
-//BENCHMARK(benchy)->ReportAggregatesOnly(1)->Repetitions(1);
-BENCHMARK(create_destroy);
-BENCHMARK(iterate_basic);
+#if 0
+BENCHMARK(create_destroy)
+	->Unit(benchmark::kMicrosecond);
+#endif
+
+BENCHMARK(iterate_basic)
+	->RangeMultiplier(8)
+	->Ranges({{1, 512}, {0, 2}});
 
 
 BENCHMARK_MAIN()
