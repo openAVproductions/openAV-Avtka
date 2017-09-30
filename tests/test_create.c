1b36c5f821b9680a03d14caa917be695835d53cd
Harry van Haaren <harryhaaren@gmail.com>
2017-09-30 17:26:11 +0100

    tests: warn users when running tests directly
    Correct usage of the test binaries is to run "ninja test"
    
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- tests/test_create.c
+++ tests/test_create.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
+
 #include "avtka.h"
 
 #define CHECK_EQUALS(x, y) do { if(x != y) return -1; } while (0)
 
 int main()
 {
+	printf("EXPECTED PRINTS OF ERRORS BELOW, PLEASE RUN TESTS USING:\n"
+	       " $ ninja test\n");
 	struct avtka_t *a;
 	a = avtka_create(0, 0);
 	CHECK_EQUALS(a, 0);
@@ -25,5 +29,7 @@ int main()
 	int32_t ret = avtka_destroy(a);
 	CHECK_EQUALS(ret, 0);
 
+	printf("EXPECTED PRINTS OF ERRORS BELOW, PLEASE RUN TESTS USING:\n"
+	       " $ ninja test\n");
 	return 0;
 }
