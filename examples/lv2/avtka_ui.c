94df2532d53a134d2fcb0680218bacfe66a99151
Harry van Haaren <harryhaaren@gmail.com>
2017-11-24 23:20:49 +0000

    example/lv2: reworked amp as example for AVTKA
    This commit changes the AVTKA example plugin into its own
    version of the LV2 supplied eg:amp plugin (with its own URI).
    This allows the AVTKA slider to control gain (showing how it
    interacts with DSP) while also demonstrating how AVTKA works.
    
    This fixes #4, thanks @m4l3z for requesting an LV2 ui example.
    
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- examples/lv2/avtka_ui.c
+++ examples/lv2/avtka_ui.c
@@ -4,24 +4,39 @@
 
 #include <avtka.h>
 
+#include "common.h"
+
 #include "lv2/lv2plug.in/ns/lv2core/lv2.h"
 #include "lv2/lv2plug.in/ns/extensions/ui/ui.h"
-
-#define ROOMY_DSP_URI "http://www.openavproductions.com/artyfx#roomy"
-#define ROOMY_UI_URI  "http://www.openavproductions.com/artyfx#roomy/gui_avtka"
 
 /* The "instance" of the UI. A pointer is passed around as the "LV2UI_Widget"
  * to let you get back to the structure itself */
 struct demo_t {
 	uint32_t dial_item;
 	struct avtka_t *avtka;
-};
 
+	/* LV2 method to write to the DSP ports from the UI:
+	 * - A function pointer to call when writing
+	 * - A "controller" (aka, handle to pass to write)
+	 */
+	LV2UI_Write_Function write_function;
+	LV2UI_Controller controller;
+};
 
 void event_cb(struct avtka_t *avtka, uint32_t item, float v, void *userdata)
 {
 	struct demo_t *demo = userdata;
+
+	/* handle the event here */
 	printf("event on item %d, value %f\n", item, v);
+
+	/* invert fader: "up" means  1.0f in avtka, while we want 0dB gain.
+	 * "down" in avtka means 0.f, while we want -70dB or so.. */
+	v = -((1-v) * 70);
+
+	/* call the write function (as LV2 host provided), passing back in
+	 * the "controller", so it can reference its own data structures */
+	demo->write_function(demo->controller, AMP_GAIN, sizeof(float), 0, &v);
 }
 
 static LV2UI_Handle
@@ -36,32 +51,31 @@ artyfx_instantiate(const struct _LV2UI_Descriptor * descriptor,
 	LV2UI_Resize* resize = NULL;
 	avtka_native_t parent_handle = 0;
 	for (int i = 0; features[i]; ++i) {
-		printf("Feature %s\n", features[i]->URI );
+		//printf("Feature %s\n", features[i]->URI );
 		if (!strcmp(features[i]->URI, LV2_UI__parent)) {
 			parent_handle = (avtka_native_t)features[i]->data;
-			printf("\tParent X11 ID %ld\n", parent_handle );
+			//printf("\tParent X11 ID %ld\n", parent_handle );
 		} else if (!strcmp(features[i]->URI, LV2_UI__resize)) {
 			resize = (LV2UI_Resize*)features[i]->data;
-			printf("\tResize %p\n", resize);
+			//printf("\tResize %p\n", resize);
 		}
 	}
 
-	if (strcmp(plugin_uri, ROOMY_DSP_URI) != 0)
+	if (strcmp(plugin_uri, AVTKA_DSP_URI) != 0)
 		return 0;
 
-	printf("matched %s\n", plugin_uri);
 	struct demo_t *demo = calloc(1, sizeof(struct demo_t));
 	if(!demo)
 		return 0;
 
 	struct avtka_opts_t opts = {
 		.native_parent = parent_handle,
-		.w = 360,
+		.w = 160,
 		.h = 240,
 		.event_callback = event_cb,
-		.event_callback_userdata = &demo,
+		.event_callback_userdata = demo,
 	};
-	demo->avtka = avtka_create("AVTKA v0.1", &opts);
+	demo->avtka = avtka_create("Avtka LV2 UI Demo", &opts);
 	if(!demo->avtka) {
 		printf("failed to create avtka instance, quitting\n");
 		return 0;
@@ -69,16 +83,16 @@ artyfx_instantiate(const struct _LV2UI_Descriptor * descriptor,
 
 	struct avtka_item_opts_t item = {
 		.name = "Dial 1",
-		.x = 10, .y = 10, .w = 50, .h = 50,
-		.draw = AVTKA_DRAW_DIAL,
+		.x = 10, .y = 10, .w = 50, .h = 150,
+		.draw = AVTKA_DRAW_SLIDER,
 		.interact = AVTKA_INTERACT_DRAG_V,
 	};
 	uint32_t dial_item_id = avtka_item_create(demo->avtka, &item);
 
-	/* TODO: add write/controller to UI to update state later..
-	ui->write_function = write_function;
-	ui->controller     = controller;
-	*/
+	demo->write_function = write_function;
+	demo->controller     = controller;
+
+	printf("%p %p\n", write_function, demo->write_function);
 
 	*widget = (void*)avtka_get_native_handle(demo->avtka);
 
@@ -131,7 +145,7 @@ artyfx_extension_data(const char* uri)
 
 static const LV2UI_Descriptor descriptor[] = {
 	{
-		ROOMY_UI_URI,
+		AVTKA_UI_URI,
 		artyfx_instantiate,
 		artyfx_cleanup,
 		artyfx_port_event,
